7bd8f80d9a06cf8bd25cdd4d2300edb140e35f0a
AdelekeVanessa <adelekevanessa2@gmail.com>
2025-04-08 07:07:51 +0300

    Modifications: Changes were made to the Tests/Banner.test.js and src/App.css files

--- Tests/Banner.test.js
+++ Tests/Banner.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Banner from "../src/components/InterractiveBanner";
+import Banner from "../src/components/InteractiveBanner";
 
 test("renders banner with correct text", () => {
   render(<Banner text="Test Banner" bg="bg-red-500" image="test.jpg" />);
